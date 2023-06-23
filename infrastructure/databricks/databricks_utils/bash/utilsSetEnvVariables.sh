f3e1ed9016daa22f2b80b0d55c84db7841f9d9b6
vuonghoainam <vuonghoainam.work@gmail.com>
2023-06-23 15:59:32 +0700

    fix: Update environment population script

--- infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
+++ infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
@@ -11,7 +11,7 @@ RESOURCE_GROUP_NAME=$( jq -r '.parameters.resourceGroupName.value' <<< "$JSON")
 echo "Resource Group Name: $RESOURCE_GROUP_NAME"
 
 DATABRICKS_WS_NAME=$( az databricks workspace list -g $RESOURCE_GROUP_NAME --query [].name -o tsv )
-AML_WS_NAME=$(az ml workspace list -g $RESOURCE_GROUP_NAME  --query [].workspaceName -o tsv)
+AML_WS_NAME=$(az ml workspace list -g $RESOURCE_GROUP_NAME  --query [].name -o tsv)
 DATABRICKS_ORDGID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].workspaceId" -o tsv)
 DATABRICKS_INSTANCE="$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].workspaceUrl" -o tsv)"
 WORKSPACE_ID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].id" -o tsv)
@@ -52,8 +52,8 @@ if [[ $DevOps_Agent == "GitHub" ]]; then
     echo "Set Databricks Host As Environment Variable..."
     echo "DATABRICKS_HOST=https://$DATABRICKS_INSTANCE" >> $GITHUB_ENV
 
-    #echo "Set Databricks Token ID As Environment Variable..."
-    #echo "DATABRICKS_TOKEN=$DATABRICKS_TOKEN" >> $GITHUB_ENV
+    echo "Set Databricks Token ID As Environment Variable..."
+    echo "DATABRICKS_TOKEN=$DATABRICKS_TOKEN" >> $GITHUB_ENV
 
     echo "Set SUBSCRIPTION_ID As Environment Variable..."
     echo "SUBSCRIPTION_ID=$SUBSCRIPTION_ID" >> $GITHUB_ENV
@@ -63,42 +63,3 @@ if [[ $DevOps_Agent == "GitHub" ]]; then
 
     echo "Set DATABRICKS_WS_NAME As Environment Variable..."
     echo "DATABRICKS_WS_NAME=$DATABRICKS_WS_NAME" >> $GITHUB_ENV
-    
-else
-
-    # Creation Of Important Environment Variables For Later Steps.
-    echo "Set Environment Variables For Later Stages..."
-
-
-    echo "ENVIRONMENT Name As Environment Variable..."
-    echo "##vso[task.setvariable variable="ENVIRONMENT";isOutput=true;]$ENVIRONMENT"
-
-
-    echo "Resource Group Name As Environment Variable..."
-    echo "##vso[task.setvariable variable="RESOURCE_GROUP_NAME";isOutput=true;]$RESOURCE_GROUP_NAME"
-
-    echo "Set Key Vault Name As Environment Variable..."
-    echo "##vso[task.setvariable variable="AZ_KEYVAULT_NAME";isOutput=true;]$AZ_KEYVAULT_NAME"
-
-    echo "Set Databricks OrgID As Environment Variable..."
-    echo "##vso[task.setvariable variable="DATABRICKS_ORDGID";isOutput=true;]$DATABRICKS_ORDGID"
-
-    echo "Set Workspace ID As Environment Variable..."
-    echo "##vso[task.setvariable variable="WORKSPACE_ID";isOutput=true;]$WORKSPACE_ID"
-
-
-    echo "Set Datbricks Instance As Environment Variable..."
-    echo "##vso[task.setvariable variable="DATABRICKS_INSTANCE";isOutput=true;]$DATABRICKS_INSTANCE"
-
-    echo "Set Databricks Host As Environment Variable..."
-    echo "##vso[task.setvariable variable="DATABRICKS_HOST";isOutput=true;]https://$DATABRICKS_INSTANCE"
-
-    echo "Set Databricks Host As Environment Variable..."
-    echo "##vso[task.setvariable variable="SUBSCRIPTION_ID";isOutput=true;]$SUBSCRIPTION_ID"
-
-    echo "Set AML_WS_NAME As Environment Variable..."
-    echo "##vso[task.setvariable variable="AML_WS_NAME";isOutput=true;]$AML_WS_NAME"
-
-    echo "Set DATABRICKS_WS_NAME As Environment Variable..."
-    echo "##vso[task.setvariable variable="DATABRICKS_WS_NAME";isOutput=true;]$DATABRICKS_WS_NAME"
-fi
