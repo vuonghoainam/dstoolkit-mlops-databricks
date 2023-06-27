1ea3d1672885a3dedc9ae8d689c541bd00dd2ed4
vuonghoainam <vuonghoainam.work@gmail.com>
2023-06-27 17:21:27 +0700

    hack: Update env var

--- infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
+++ infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
@@ -13,28 +13,28 @@ if [[ $DevOps_Agent == "GitHub" ]]; then
     echo "ENVIRONMENT=$ENVIRONMENT" >> $GITHUB_ENV
 
     echo "Set Resource Group Name Name As Environment Variable..."
-    echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME" >> $GITHUB_ENV
+    echo "RESOURCE_GROUP_NAME=aimlexperimentdbspremium" >> $GITHUB_ENV
 
     echo "Set Databricks OrgID As Environment Variable..."
-    echo "DATABRICKS_ORDGID=$DATABRICKS_ORDGID" >> $GITHUB_ENV
+    echo "DATABRICKS_ORDGID=5089469696695010" >> $GITHUB_ENV
 
     echo "Set Workspace ID As Environment Variable..."
-    echo "WORKSPACE_ID=$WORKSPACE_ID" >> $GITHUB_ENV
+    echo "WORKSPACE_ID=/subscriptions/8178e63d-3cfe-4434-bae7-4bac02afc65f/resourceGroups/AIMLPlatformExperiment/providers/Microsoft.Databricks/workspaces/aimlexperimentdbspremium" >> $GITHUB_ENV
 
     echo "Set Datbricks Instance As Environment Variable..."
-    echo "DATABRICKS_INSTANCE=$DATABRICKS_INSTANCE" >> $GITHUB_ENV
+    echo "DATABRICKS_INSTANCE=adb-5089469696695010.10.azuredatabricks.net" >> $GITHUB_ENV
 
     echo "Set Databricks Host As Environment Variable..."
-    echo "DATABRICKS_HOST=https://$DATABRICKS_INSTANCE" >> $GITHUB_ENV
+    echo "DATABRICKS_HOST=https://adb-5089469696695010.10.azuredatabricks.net" >> $GITHUB_ENV
 
     echo "Set Databricks Token ID As Environment Variable..."
     echo "DATABRICKS_TOKEN=$DATABRICKS_TOKEN" >> $GITHUB_ENV
 
     echo "Set SUBSCRIPTION_ID As Environment Variable..."
-    echo "SUBSCRIPTION_ID=$SUBSCRIPTION_ID" >> $GITHUB_ENV
+    echo "SUBSCRIPTION_ID=8178e63d-3cfe-4434-bae7-4bac02afc65f" >> $GITHUB_ENV
 
     echo "Set AML_WS_NAME As Environment Variable..."
-    echo "AML_WS_NAME=$AML_WS_NAME" >> $GITHUB_ENV
+    echo "AML_WS_NAME=aimlexperiment" >> $GITHUB_ENV
 
     echo "Set DATABRICKS_WS_NAME As Environment Variable..."
-    echo "DATABRICKS_WS_NAME=$DATABRICKS_WS_NAME" >> $GITHUB_ENV
+    echo "DATABRICKS_WS_NAME=aimlexperimentdbspremium" >> $GITHUB_ENV
