1c1bf39749751ec3e33d06fa82879e594853ceae
vuonghoainam <vuonghoainam.work@gmail.com>
2023-06-27 16:44:32 +0700

    chore(ci): change secrets to env in github action

--- infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
+++ infrastructure/databricks/databricks_utils/bash/utilsSetEnvVariables.sh
@@ -10,23 +10,6 @@ JSON=$( jq '.' infrastructure/bicep/params/$ENVIRONMENT/bicep.parameters.json)
 RESOURCE_GROUP_NAME=$( jq -r '.parameters.resourceGroupName.value' <<< "$JSON")
 echo "Resource Group Name: $RESOURCE_GROUP_NAME"
 
-DATABRICKS_WS_NAME=$( az databricks workspace list -g $RESOURCE_GROUP_NAME --query [].name -o tsv )
-AML_WS_NAME=$(az ml workspace list -g $RESOURCE_GROUP_NAME  --query [].name -o tsv)
-DATABRICKS_ORDGID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].workspaceId" -o tsv)
-DATABRICKS_INSTANCE="$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].workspaceUrl" -o tsv)"
-WORKSPACE_ID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].id" -o tsv)
-AZ_KEYVAULT_NAME=$(az keyvault list -g $RESOURCE_GROUP_NAME --query "[].name" -o tsv)
-SUBSCRIPTION_ID=$( az account show --query id -o tsv )
-#echo $SUBSCRIPTION_ID
-#echo $DATABRICKS_ORDGID
-#echo $WORKSPACE_ID
-#echo $AZ_KEYVAULT_NAME
-#echo $SUBSCRIPTION_ID
-#echo $AML_WS_NAME
-#echo $DATABRICKS_WS_NAME
-#DATABRICKS_TOKEN=$(az keyvault secret show --name "dbkstoken" --vault-name $AZ_KEYVAULT_NAME --query "value" -o tsv)
-
-
 if [[ $DevOps_Agent == "GitHub" ]]; then
     # Creation Of Important Environment Variables For Later Steps.
     echo "Set Environment Variables For Later Stages..."
@@ -36,9 +19,6 @@ if [[ $DevOps_Agent == "GitHub" ]]; then
 
     echo "Set Resource Group Name Name As Environment Variable..."
     echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME" >> $GITHUB_ENV
-
-    echo "Set Key Vault Name As Environment Variable..."
-    echo "AZ_KEYVAULT_NAME=$AZ_KEYVAULT_NAME" >> $GITHUB_ENV
 
     echo "Set Databricks OrgID As Environment Variable..."
     echo "DATABRICKS_ORDGID=$DATABRICKS_ORDGID" >> $GITHUB_ENV
